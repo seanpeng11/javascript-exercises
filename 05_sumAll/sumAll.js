20d935202e76a54b525f6515fa3a8d9bd6b14272
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-20 20:29:32 +0800

    Add solution to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof start !== "number" || typeof end !== "number") {
+        return "ERROR";
+    }
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (start > end) {
+        return sumAll(end, start);
+    }
+    return (start + end) * (end - start + 1) / 2;
 };
 
 // Do not edit below this line
