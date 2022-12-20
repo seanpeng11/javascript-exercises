22582c1797d96a1f1a11ae8f3c03efb6d96c5edd
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-20 20:18:25 +0800

    Add solution to exercise04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let idx;
+    for (const arg of args) {
+        idx = arr.indexOf(arg);
+        if (idx === -1) continue;
+        arr.splice(idx, 1)
+    }
+    return arr;
 };
 
 // Do not edit below this line
