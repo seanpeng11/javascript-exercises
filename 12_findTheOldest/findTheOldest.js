0d06b7389c370f56e52f65fe2a05aae49c138d13
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-24 17:37:03 +0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce(function(a, b) {
+        let aAge = ("yearOfDeath" in a ? a.yearOfDeath : new Date().getFullYear()) - a.yearOfBirth;
+        let bAge = ("yearOfDeath" in b ? b.yearOfDeath : new Date().getFullYear()) - b.yearOfBirth;
+        return aAge > bAge ? a : b;
+    })
 };
 
 // Do not edit below this line
