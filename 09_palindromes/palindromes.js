429b595e6545d22c8a3649d3c03debe91245bbc4
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-24 17:10:54 +0800

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    s = s.toLowerCase();
+    let left = 0, right = s.length - 1
+    while (left < right) {
+        while (!((/[a-z]/i).test(s.charAt(left))) && left < right) {
+            left++;
+        }
+        while (!((/[a-z]/i).test(s.charAt(right))) && right > left) {
+            right--;
+        }
+        if (s.charAt(left) !== s.charAt(right)) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
