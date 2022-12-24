b2952122994408391e1f8437ee417fcc20084bae
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-24 17:18:51 +0800

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n <= 0) {
+        return "OOPS";
+    }
+    if (n < 3) {
+        return 1;
+    }
+    let cur = 1, prev = 1, tmp = 0;
+    for (let i = 3; i <= n; i++) {
+        tmp = cur
+        cur += prev;
+        prev = tmp;
+    }
+    return cur;
 };
 
 // Do not edit below this line
