cbaf94f7506537b9fb5a9247e96a76341763f24c
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-24 12:54:32 +0800

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let total = 1;
+  for (let i = 2; i <= n; i++) {
+    total *= i;
+  }
+  return total
 };
 
 // Do not edit below this line
